a4bc3e5f3489eae87f1e46584bfa29c4cb827c90
nasimdaghash <87161661+nasimdaghash@users.noreply.github.com>
2021-09-14 13:42:21 +0300

    objects in javascript

--- objects/7.2_object__methods.js
+++ objects/7.2_object__methods.js
@@ -5,10 +5,21 @@ const mycountry={
     language: 'hebrow' ,
     population :' 9 million' ,
     neighbours : ['egypt','jordan','syria','Lebanon'],
-    }
 
-mycountry.describe =function(){
-    console.log(`${this.country} has ${this.population} people, there mother tongue is ${this.language} they have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+    describe : function(){
+        console.log(`${this.country} has ${this.population} people, there mother tongue is ${this.language} they have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+    },
+
+    checkIsland : function(){
+        if (this.neighbours.length > 0)
+            this.isIsland=false;
+        else
+            this.isIsland=true;       
+    }
 }
 
+
 mycountry.describe();
+mycountry.checkIsland();
+console.log(mycountry);
+
